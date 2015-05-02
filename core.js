bd1e81f1e9675203c92470a0a30b94ffb3ded14f
SerperiorBae <kevin.mcneill@live.co.uk>
2015-05-02 15:53:28 +0100

    Update core.js

--- core.js
+++ core.js
@@ -91,8 +91,52 @@ emoticons: {
 	}
 },        
 
-	
+  poll: function () {
+        var poll = {};
+        var components = {
+
+            reset: function (roomId) {
+                poll[roomId] = {
+                    question: undefined,
+                    optionList: [],
+                    options: {},
+                    display: '',
+                    topOption: ''
+                };
+            },
+
+            splint: function (target) {
+                var parts = target.split(',');
+                var len = parts.length;
+                while (len--) {
+                    parts[len] = parts[len].trim();
+                }
+                return parts;
+            }
+
+        };
+
+        for (var i in components) {
+            if (components.hasOwnProperty(i)) {
+                poll[i] = components[i];
+            }
+        }
+
+        for (var id in Rooms.rooms) {
+            if (Rooms.rooms[id].type === 'chat' && !poll[id]) {
+                poll[id] = {};
+                poll.reset(id);
+            }
+        }
+
+        return poll;
+    },
+
 };
+
+
+
+
 function MD5(f) {
     function i(b, c) {
         var d, e, f, g, h;

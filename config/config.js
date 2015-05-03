a82e724883875da4b8fcc6b7a35b9ea4f05a15ec
DarkSuicune <ckasslertaub@pclcnj.com>
2015-05-03 14:36:42 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -165,6 +165,7 @@ exports.customavatars = {
 	'philmiester': '93jm23N.png',
 	'shodaimamoru': '7wjb6cB.png',
 	'hayleysworld': 'Hayley.png',
+	'fawnie': 'duQYVTf.png',
 	'victette': 'Qy9Rkw.gif'
 };
 

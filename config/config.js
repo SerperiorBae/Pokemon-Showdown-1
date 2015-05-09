076ed57e312a4256fa6e5f3ac9e06b9367e17504
DarkSuicune <ckasslertaub@pclcnj.com>
2015-05-09 13:43:27 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -168,6 +168,7 @@ exports.customavatars = {
 	'shawott': '11247257_299517953551939_1011576751_n1.gif',
 	'situmparletmort': '9GrQW8g.png',
 	'morganzedriftking': 'photo.jpg.png',
+	'old90skid': 'reagan.gif',
 	'nekis13': 'hO453yM.jpg',
 	'solorbeam': 'solorbeam.gif',
 	'blazingdark': 'crmxKWO.png',

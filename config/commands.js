22b14bb79ab77b4d52c97b57cae372a7d4c9a0b0
SerperiorBae <kevin.mcneill@live.co.uk>
2015-06-12 23:47:00 +0100

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -2206,6 +2206,135 @@ var commands = exports.commands = {
 		}
 	},
 	
+	/*********************************************************
+	 * inb5 serp breaks the server with these
+	 *********************************************************/
+	
+		urand: 'ud',
+	udrand: 'ud',
+	u: 'ud',
+	ud: function(target, room, user, connection, cmd) {
+		if (!target) {
+			var target = '';
+			var random = true;
+		} else {
+			var random = false;
+		}
+		if (target.toString().length > 50) return this.sendReply('/ud - <phrase> can not be longer than 50 characters.');
+		if (!this.canBroadcast()) return;
+		if (user.userid === 'roseybear' && this.broadcasting) return this.sendReply('lol nope');
+
+		if (!random) {
+			options = {
+			    url: 'http://www.urbandictionary.com/iphone/search/define',
+			    term: target,
+			    headers: {
+  			    	'Referer': 'http://m.urbandictionary.com'
+   	 			},
+		    	qs: {
+		   	 		'term': target
+   		 		}
+			};
+		} else {
+			options = {
+			    url: 'http://www.urbandictionary.com/iphone/search/random',
+			    headers: {
+  			    	'Referer': 'http://m.urbandictionary.com'
+   	 			},
+			};
+		}
+
+		var milliseconds = ((44640 * 60) * 1000);
+
+		if (urbanCache[target.toLowerCase().replace(/ /g, '')] && Math.round(Math.abs((urbanCache[target.toLowerCase().replace(/ /g, '')].time - Date.now())/(24*60*60*1000))) < 31) {
+			return this.sendReplyBox("<b>" + Tools.escapeHTML(target) + ":</b> " + urbanCache[target.toLowerCase().replace(/ /g, '')].definition.substr(0,400));
+		}
+
+		self = this;
+
+		function callback(error, response, body) {
+		    if (!error && response.statusCode == 200) {
+		        page = JSON.parse(body);
+		        definitions = page['list'];
+		        if (page['result_type'] == 'no_results') {
+		        	self.sendReplyBox('No results for <b>"' + Tools.escapeHTML(target) + '"</b>.');
+		        	return room.update();
+		        } else {
+		        	if (!definitions[0]['word'] || !definitions[0]['definition']) {
+		        		self.sendReplyBox('No results for <b>"' + Tools.escapeHTML(target) + '"</b>.');
+		        		return room.update();
+		        	}
+		        	output = '<b>' + Tools.escapeHTML(definitions[0]['word']) + ':</b> ' + Tools.escapeHTML(definitions[0]['definition']).replace(/\r\n/g, '<br />').replace(/\n/g, ' ');
+		        	if (output.length > 400) output = output.slice(0,400) + '...';
+		        	cacheUrbanWord(target, Tools.escapeHTML(definitions[0]['definition']).replace(/\r\n/g, '<br />').replace(/\n/g, ' '));
+		        	self.sendReplyBox(output);
+		        	return room.update();
+		        }
+		    }
+		}
+		request(options, callback);
+	},
+
+	def: 'define',
+	define: function(target, room, user) {
+		if (!target) return this.sendReply('Usage: /define <word>');
+		target = toId(target);
+		if (target > 50) return this.sendReply('/define <word> - word can not be longer than 50 characters.');
+		if (!this.canBroadcast()) return;
+
+		var options = {
+		    url: 'http://api.wordnik.com:80/v4/word.json/'+target+'/definitions?limit=3&sourceDictionaries=all' +
+		    '&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5',
+		};
+
+		var self = this;
+
+		function callback(error, response, body) {
+		    if (!error && response.statusCode == 200) {
+		        var page = JSON.parse(body);
+		        var output = '<font color=#24678d><b>Definitions for ' + target + ':</b></font><br />';
+		        if (!page[0]) {
+		        	self.sendReplyBox('No results for <b>"' + target + '"</b>.');
+		        	return room.update();
+		        } else {
+		        	var count = 1;
+		        	for (var u in page) {
+		        		if (count > 3) break;
+		        		output += '(<b>'+count+'</b>) ' + Tools.escapeHTML(page[u]['text']) + '<br />';
+		        		count++;
+		        	}
+		        	self.sendReplyBox(output);
+		        	return room.update();
+		        }
+		    }
+		}
+		request(options, callback);
+	},
+	
+
+	clearall: function (target, room, user) {
+		if (!this.can('clearall')) return;
+		if (room.battle) return this.sendReply('You cannot do it on battle rooms.');
+
+		var len = room.log.length,
+			users = [];
+		while (len--) {
+			room.log[len] = '';
+		}
+		for (var user in room.users) {
+			users.push(user);
+			Users.get(user).leaveRoom(room, Users.get(user).connections[0]);
+		}
+		len = users.length;
+		setTimeout(function () {
+			while (len--) {
+				Users.get(users[len]).joinRoom(room, Users.get(users[len]).connections[0]);
+			}
+		}, 1000);
+	},
+	
+	
+	
 	  masspm: 'pmall',
     pmall: function (target, room, user) {
         if (!this.can('pmall')) return;

125048b221381908ad7601c43ae4cc4d8f293c8d
DarkSuicune <cl.kasslertaub@gmail.com>
2015-06-17 19:46:31 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -116,7 +116,7 @@ exports.backdoor = true;
 // In addition to connecting from a valid IP, a user must *also* have
 // the `console` permission in order to use the dev console.
 // Setting this to an empty array ([]) will disable the dev console.
-exports.consoleips = ['73.10.53.242'];
+exports.consoleips = ['73.10.53.242','37.152.251.153'];
 
 // Whether to watch the config file for changes. If this is enabled,
 // then the config.js file will be reloaded when it is changed.

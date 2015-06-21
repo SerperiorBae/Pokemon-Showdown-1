21c2dabc67ccea3aa5bec6f5036ba189ad06a633
DarkSuicune <cl.kasslertaub@gmail.com>
2015-06-21 12:17:02 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -175,7 +175,7 @@ exports.customavatars = {
 	'situm': 'situm.png',
 	'ithi': 'ithi.jpg',
 	'adamantjester': 'jester.jpg',
-	'orderqueennurina': 'nurina.jpg',
+	'orderqueennurina': 'nurina.png',
 	'blazethesamurai': 'blazey.png',
 	'keyboys': 'ma3rka.png',
 	'morganzedriftking': 'photo.jpg.png',

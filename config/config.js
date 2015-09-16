efa3923fc7587795a9ef918793939dd357a50205
DarkSuicune <cl.kasslertaub@gmail.com>
2015-09-15 21:40:40 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -166,6 +166,7 @@ exports.customavatars = {
 	'mystifi': 'vVGPiMK.png',
 	'nekis13': '5UhFtHc-1.png',
 	'narcomania': 'serpy.gif',
+	'lflutterbotl': 'wj35qCh.gif',
 	'lfluttershyl': 'VD6fqII.png',
 };
 
